7a9e8c6ff48c018ad3293f2e4c094292e574f64b
Ishaan Harry Kavoori <ishaanharry@live.com>
2020-11-13 17:57:12 -0800

    baseline implementation of calendar UI via React Scheduler library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
 function App() {
+
+  const currentDate = '2018-11-01';
+  const schedulerData = [
+    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper>
+      <Scheduler
+        data={schedulerData}
+      >
+        <ViewState
+          currentDate={currentDate}
+        />
+        <DayView
+          startDayHour={9}
+          endDayHour={14}
+        />
+        <Appointments />
+      </Scheduler>
+    </Paper>
   );
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
